30d482a8954e1422ba7539ff8ef6d37a1b823107
Jack Werner <its.jack.werner@gmail.com>
2023-06-28 10:49:29 -0400

    got a basic name animation working

---
+++ src/app/components/NameCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles/name-card.module.css";
+
+export const NameCard = () => {
+    return (
+        <div className={styles.name}>
+            <h1>JACK WERNER</h1>
+        </div>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/rly1kmu.css"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
