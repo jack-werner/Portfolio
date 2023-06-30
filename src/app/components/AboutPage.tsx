13b09307032c667152b6a43a2fa42a8b126f6de0
Jack Werner <its.jack.werner@gmail.com>
2023-06-30 13:58:09 -0400

    added animation for image to trigger when it is visible. currently the visibility is not detecting properly

--- src/app/components/AboutPage.tsx
+++ src/app/components/AboutPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ImageCard } from "./ImageCard";
 import styles from "./styles/about-page.module.css";
 import { TextCard } from "./TextCard";
 
@@ -7,24 +8,25 @@ export const AboutPage = () => {
         <div className={styles.background}>
             {/* <Photocard/>
              */}
-            <img className={styles.image}
+            {/* <img className={styles.image}
                 src="https://i.imgur.com/nZwPqgx.jpg"  
                 alt="Jack Werner Portfolio Picture"
-            />
+            /> */}
+            <ImageCard></ImageCard>
             <TextCard title="About Me"> 
-            <p>
-                Hello! I'm Jack Werner, a Data/Full stack software engineer based in New York City. 
-                I have professional experience in <strong> Data Science</strong> and 
-                <strong> Data Engineering</strong>, and personal experience with 
-                <strong> Web Development</strong>, I built this site you're looking at right now! 
-                I am passionate about using software to enhance the joy discovering, learning 
-                about, and enjoying music. You can read about my different projects that I have 
-                undertaken in my blog.
-            </p>
-            <p>
-                In my personal life I also enjoy DJing and producing music. I also enjoy 
-                photography, Generative AI artwork, Surfing and snowboarding.
-            </p>
+                <p>
+                    Hello! I'm Jack Werner, a Data/Full stack software engineer based in New York City. 
+                    I have professional experience in <strong> Data Science</strong> and 
+                    <strong> Data Engineering</strong>, and personal experience with 
+                    <strong> Web Development</strong>, I built this site you're looking at right now! 
+                    I am passionate about using software to enhance the joy discovering, learning 
+                    about, and enjoying music. You can read about my different projects that I have 
+                    undertaken in my blog.
+                </p>
+                <p>
+                    In my personal life I also enjoy DJing and producing music. I also enjoy 
+                    photography, Generative AI artwork, Surfing and snowboarding.
+                </p>
             </TextCard>
         </div>
     )
