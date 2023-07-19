b74c6f01faa0c36a20a36ee026c20f6d94fa1576
Jack Werner <its.jack.werner@gmail.com>
2023-07-19 09:44:01 -0700

    fixed transition for the text card

--- src/app/components/TextCard.tsx
+++ src/app/components/TextCard.tsx
@@ -1,5 +1,6 @@
-import React, {ReactNode} from "react";
-import styles from './styles/text-card.module.css'
+import React, {ReactNode, useState, useEffect, useRef} from "react";
+import styles from './styles/text-card.module.css';
+import { motion } from 'framer-motion';
 
 interface TextCardProps {
     title: string;
@@ -7,14 +8,61 @@ interface TextCardProps {
 }
 
 export const TextCard: React.FC<TextCardProps> = ({title, children}) => {
+    const textRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                } else {
+                    setIsVisible(false);
+                }
+            });
+        });
+        
+        if (textRef.current) {
+            observer.observe(textRef.current);
+        }
+
+        return () => {
+            if (textRef.current) {
+                observer.unobserve(textRef.current);
+            }
+        }
+        
+    }, []);
+
+    const variants = {
+        hidden: {
+            x: '100%',
+            opacity: 0.5,
+            filter: 'blur(20px)',
+        }, 
+        visible: {
+            x: 0,
+            opacity: 1,
+            filter: 'blur(0)',
+            transition: {
+                duration: 1,
+            }
+        }
+    }
+
     return (
-        <div className={styles.card}>
+        <motion.div className={styles.card}
+            initial="hidden"
+            animate={isVisible ? 'visible': 'hidden'}
+            variants={variants}
+            ref={textRef}
+        >
             <div className={styles.header}>
                 {title}
             </div>
             <div className={styles.body}>
                 {children}
             </div>
-        </div>
+        </motion.div>
     )  
 }
