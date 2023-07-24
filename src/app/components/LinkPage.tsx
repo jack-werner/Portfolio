d60c38dfbe2df692f4b351c3dd91fc5ff1d5bae1
Jack Werner <its.jack.werner@gmail.com>
2023-07-24 13:55:47 -0700

    made link card its own component and added hover animations

--- src/app/components/LinkPage.tsx
+++ src/app/components/LinkPage.tsx
@@ -1,13 +1,14 @@
-import styles from "./styles/link-page.module.css"
+import { LinkCard } from "./LinkCard";
+import styles from "./styles/link-page.module.css";
 
 export const LinkPage = () => {
     return (
         <div className={styles.background}>
             <div className={styles.container}>
-                <div className={styles.link}>Software</div>
-                <div className={styles.link}>Blog</div>
-                <div className={styles.link}>Music</div>
-                <div className={styles.link}>Art</div>
+                <LinkCard text="Software" link=""/>
+                <LinkCard text="Blog" link=""/>
+                <LinkCard text="Music" link=""/>
+                <LinkCard text="Art" link=""/>
             </div>
         </div>
     )
