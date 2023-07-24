2480d5d6e48ac74057faf769813550fe7f7d7db5
Jack Werner <its.jack.werner@gmail.com>
2023-07-24 11:58:41 -0700

    added link structure

--- src/app/components/LinkPage.tsx
+++ src/app/components/LinkPage.tsx
@@ -3,11 +3,12 @@ import styles from "./styles/link-page.module.css"
 export const LinkPage = () => {
     return (
         <div className={styles.background}>
-            <div className={styles.link}>yo</div>
-            <div className={styles.link}>yo</div>
-            <div className={styles.link}>yo</div>
-            <div className={styles.link}>yo</div>
-
+            <div className={styles.container}>
+                <div className={styles.link}>Software</div>
+                <div className={styles.link}>Blog</div>
+                <div className={styles.link}>Music</div>
+                <div className={styles.link}>Art</div>
+            </div>
         </div>
     )
 }
