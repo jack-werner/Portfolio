f27e6d022aaca005f2db485cabfe098877869eb0
Jack Werner <its.jack.werner@gmail.com>
2023-07-24 17:07:15 -0700

    added soundcloud link

--- src/app/components/LinkPage.tsx
+++ src/app/components/LinkPage.tsx
@@ -7,7 +7,7 @@ export const LinkPage = () => {
             <div className={styles.container}>
                 <LinkCard text="Software" link=""/>
                 <LinkCard text="Blog" link=""/>
-                <LinkCard text="Music" link=""/>
+                <LinkCard text="Music" link="https://soundcloud.com/jack-wolfgang-music"/>
                 <LinkCard text="Art" link=""/>
             </div>
         </div>
