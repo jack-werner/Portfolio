e475ba0184f025b32ce341d425774b0c7a39fdd9
Jack Werner <its.jack.werner@gmail.com>
2023-08-02 12:03:49 -0400

    this isn't perfect but it kinda works for now and I'm about to lose it

--- src/app/components/TextCard.tsx
+++ src/app/components/TextCard.tsx
@@ -3,12 +3,7 @@ import styles from './styles/text-card.module.css';
 import { motion } from 'framer-motion';
 import { ResumeDownload } from "./ResumeDownload";
 
-interface TextCardProps {
-    title: string;
-    children: ReactNode;
-}
-
-export const TextCard: React.FC<TextCardProps> = ({title, children}) => {
+export const TextCard: React.FC = () => {
     const textRef = useRef(null);
     const [isVisible, setIsVisible] = useState(false);
 
@@ -60,14 +55,30 @@ export const TextCard: React.FC<TextCardProps> = ({title, children}) => {
             ref={textRef}
         >
             <div className={styles.header}>
-                {title}
+                About Me
             </div>
-            <div className={styles.body}>
-                {children}
+            <div className={styles.scroll}>
+                <div className={styles.body}>
+                    <p>
+                        Hi! I'm Jack Werner, I'm a Software Engineer and DJ in Manhattan, NYC. 
+                        I have experience in <strong>Data Engineering, Data Science, Data Analysis, and 
+                        Web Development</strong>. I specifically enjoy working with <strong>Python, AWS, SQL, 
+                        and Next.js</strong>. I build technology that enables the creation, discovery, 
+                        and appreciation of music. Music is my greatest passion and using software 
+                        to help people enjoy it as much as I do brings me great joy. 
+                        You follow the links below to see some projects I have undertaken in this 
+                        area and click on the link below to see my Resume and view my past professional 
+                        experience.
+                    </p>
+                    <p>
+                        In my personal life I also enjoy DJing and producing music, photography, generative AI artwork, surfing and snowboarding. 
+                        Keep scrolling to take a look at some of the other work I have done.
+                    </p>
+                </div>
             </div>
             <ResumeDownload/>
+            <div className={styles.blur}></div>
         </motion.div>
-    </>
-       
+    </>       
     )  
 }
