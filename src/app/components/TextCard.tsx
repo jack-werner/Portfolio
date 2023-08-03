fb64e24c8ad35e0bebe379fe4993be104c85204c
Jack Werner <its.jack.werner@gmail.com>
2023-08-03 09:45:48 -0400

    fixed header text size I think

--- src/app/components/TextCard.tsx
+++ src/app/components/TextCard.tsx
@@ -66,13 +66,13 @@ export const TextCard: React.FC = () => {
                         and Next.js</strong>. I build technology that enables the creation, discovery, 
                         and appreciation of music. Music is my greatest passion and using software 
                         to help people enjoy it as much as I do brings me great joy. 
-                        You follow the links below to see some projects I have undertaken in this 
+                        You can follow the links below to see some projects I have undertaken in this 
                         area and click on the link below to see my Resume and view my past professional 
                         experience.
                     </p>
                     <p>
                         In my personal life I also enjoy DJing and producing music, photography, generative AI artwork, surfing and snowboarding. 
-                        Keep scrolling to take a look at some of the other work I have done.
+                        Keep scrolling to take a look at some of the other work I have done!
                     </p>
                 </div>
             </div>
