9820ca9f89cbfc41de55760a5c2db01185439692
Jack Werner <its.jack.werner@gmail.com>
2023-10-05 19:47:45 -0400

    updated blog link

--- src/app/components/LinkPage.tsx
+++ src/app/components/LinkPage.tsx
@@ -5,7 +5,7 @@ export const LinkPage = () => {
     <div className="bg-black flex flex-row flex-wrap items-center justify-center h-screen w-screen p-8">
       <div className="flex flex-row flex-shrink-0 flex-wrap items-center justify-center w-full">
         <LinkCard text="Software" link="https://github.com/jack-werner" />
-        <LinkCard text="Blog" link="https://jackwerner.substack.com" />
+        <LinkCard text="Blog" link="https://medium.com/@its.jack.werner" />
         <LinkCard
           text="Music"
           link="https://soundcloud.com/jack-wolfgang-music"
